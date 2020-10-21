c9e15994e8d54f82f3766df2e58194250deb19b5
Hrithik37 <hrithikguntuka69@gmail.com>
2020-10-22 04:17:30 +0530

    Finished project

---
+++ detect_helmet_video.py
@@ -0,0 +1,160 @@
+# import the necessary packages
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import time
+import cv2
+import os
+# audio imports
+import simpleaudio as sa
+
+
+def detect_and_predict_helmet(frame, faceNet, maskNet):
+    # grab the dimensions of the frame and then construct a blob
+    # from it
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+                                 (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+    print(detections.shape)
+
+    # initialize our list of faces, their corresponding locations,
+    # and the list of predictions from our model
+    faces = []
+    locs = []
+    preds = []
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            # add the face and bounding boxes to their respective
+            # lists
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    # only make a predictions if at least one face was detected
+    if len(faces) > 0:
+        # for faster inference we'll make batch predictions on *all*
+        # faces at the same time rather than one-by-one predictions
+        # in the above `for` loop
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
+    return (locs, preds)
+
+
+# load our serialized face detector model from disk
+prototxtPath = r"face_detector\deploy.prototxt"
+weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the helmet_detector model from disk
+maskNet = load_model("helmet_detector.model")
+
+# initialize the video stream
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+
+# loop over the frames from the video stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    # detect person in the frame and determine if they are wearing a helmet or not
+
+    (locs, preds) = detect_and_predict_helmet(frame, faceNet, maskNet)
+
+    # loop over the detected face locations and their corresponding
+    # locations
+    for (box, pred) in zip(locs, preds):
+        # unpack the bounding box and predictions
+        (startX, startY, endX, endY) = box
+        (Not_wearing_helmet, Wearing_helmet) = pred
+
+        # determine the class label and color we'll use to draw
+        # the bounding box and text
+        label = "Not_wearing_helmet"
+        if Not_wearing_helmet > Wearing_helmet:
+            color = (0, 0, 255)
+            label = "Not_wearing_helmet"
+        else:
+            label = "Wearing_helmet"
+            color = (0, 255, 0)
+
+        # beep sound if not_wearing_helmet
+        if Not_wearing_helmet > Wearing_helmet:
+            frequency = 1250  # Our played note will be 400hz
+            fs = 44100  # 44100 samples per second
+            seconds = 1  # Note duration of 3 seconds
+
+            for i in range(2):
+                for j in range(2):
+                    time.sleep(1)
+                    # Generate array with seconds*samples_rate steps, ranging between
+                    t = np.linspace(0, seconds, seconds * fs, False)
+                    # Generate a 400hz sine wave
+                    note = np.sin(frequency * t * 2 * np.pi)
+                    # Ensure that highest value is in 16-bit range
+                    audio = note * (2 ** 15 - 1) / np.max(np.abs(note))
+                    # convert to 16-bit data
+                    audio = audio.astype(np.int16)
+                    # Start playback
+                    play_obj = sa.play_buffer(audio, 1, 2, fs)
+                    # wait for the playback to finish before exiting
+                    play_obj.wait_done()
+
+        # include the probability in the label
+        label = "{}: {:.2f}%".format(label, max(
+            Wearing_helmet, Not_wearing_helmet) * 100)
+
+        # display the label and bounding box rectangle on the output
+        # frame
+        cv2.putText(frame, label, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
